1759cd5f975f2cedec47a9a2345d17310ee2faf6
Avalander <darth.quesorico@gmail.com>
2020-06-06 08:31:01 +0200

    Add more tests.

--- core/main.test.js
+++ core/main.test.js
@@ -10,8 +10,8 @@ const dummyPatch = (_, b) => b
 tap.test('Invokes patch with the given node and the return of view', t => {
 	const node = 'Onion'
 	const patch = (a, b) => {
-		t.equal(a, 'Onion')
-		t.equal(b, 'Potatoes')
+		t.equal(a, 'Onion', 'Passes the old node as first argument')
+		t.equal(b, 'Potatoes', 'Passes the result of invoking view as second argument')
 		t.end()
 	}
 
@@ -24,8 +24,8 @@ tap.test('Invokes patch with the given node and the return of view', t => {
 
 tap.test('Runs the effects returned by the init function', t => {
 	const fx = (args, dispatch) => {
-		t.deepEqual(args, [ 1, 2, 3 ])
-		t.equal(typeof dispatch, 'function')
+		t.deepEqual(args, [ 1, 2, 3 ], 'Passes the arguments to the effect')
+		t.equal(typeof dispatch, 'function', 'Passes dispatch to the effect')
 		t.end()
 	}
 	const init = () => {
@@ -46,12 +46,99 @@ tap.test('Runs the effects returned by the init function', t => {
 tap.test('Calls view with the state returned by the init function', t => {
 	const init = () => [ 'Carrots', []]
 	const view = state => {
-		t.equal(state, 'Carrots')
+		t.equal(state, 'Carrots', 'Passes the current state to the view')
 		t.end()
 	}
 
 	shadowflare(dummyPatch) ({
 		init,
+		view,
+		node: dummyNode,
+	})
+})
+
+tap.test('Dispatches actions from the view', t => {
+	const action = (state, a, b) => {
+		t.equal(state, '', 'Passes the current state to the action')
+		t.equal(a, 1, 'Passes the arguments to the action')
+		t.equal(b, 2, 'Passes the arguments to the action')
+
+		return [ 'Done' ]
+	}
+	const view = (state, dispatch) => {
+		if (state != 'Done') {
+			setTimeout(() => {
+				dispatch([ action, 1, 2 ])
+			}, 100)
+		}
+		return state
+	}
+	const patch = (_, n) => {
+		if (n == 'Done') t.end()
+		return n
+	}
+
+	shadowflare(patch) ({
+		init: emptyInit,
+		view,
+		node: dummyNode,
+	})
+})
+
+tap.test('Runs effects from an action', t => {
+	const fx = (args, dispatch) => {
+		t.equal(typeof dispatch, 'function', 'Passes dispatch as second argument to the effect')
+		t.deepEqual(args, [ 1, 2, 3 ], 'Passes the arguments to the effect')
+		t.end()
+	}
+	const action = state => {
+		return [ 'Done', [
+			[ fx, 1, 2, 3 ],
+		]]
+	}
+	const view = (state, dispatch) => {
+		if (state != 'Done') {
+			setTimeout(() => {
+				dispatch([ action ])
+			}, 100)
+		}
+		return state
+	}
+
+	shadowflare(dummyPatch) ({
+		init: emptyInit,
+		view,
+		node: dummyNode,
+	})
+})
+
+tap.test('Dispatches actions from an effect', t => {
+	const action_1 = state => {
+		const effects = [
+			[ fx, state ],
+		]
+		return [ 'Done', effects ]
+	}
+	const action_2 = (state, ...args) => {
+		t.deepEqual(args, [ 1, 2 ], 'Passes the arguments to the action triggered from the effect')
+		t.end()
+		return [ state ]
+	}
+	const fx = (args, dispatch) => {
+		t.deepEqual(args, [ '' ], 'Passes the arguments to the effect')
+		dispatch([ action_2, 1, 2 ])
+	}
+	const view = (state, dispatch) => {
+		if (state != 'Done') {
+			setTimeout(() => {
+				dispatch([ action_1 ])
+			}, 100)
+		}
+		return state
+	}
+
+	shadowflare(dummyPatch) ({
+		init: emptyInit,
 		view,
 		node: dummyNode,
 	})
